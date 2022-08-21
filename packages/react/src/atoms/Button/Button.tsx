56fb9901d5afd90b3c3c5ba06a9b94c9c4b3c3b2
sksurendrakmr <sksurendrakmr@gmail.com>
2022-08-21 20:25:09 +0530

    Basic react project setup

---
+++ packages/react/src/atoms/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+type ButtonProps = {
+    label: string;
+}
+const Button = ({ label }: ButtonProps) => {
+    return (
+        <button>{label}</button>
+    )
+}
+
+export default Button;
---
+++ packages/react/src/index.ts
@@ -0,0 +1,4 @@
+// Here we are going to export all of our components
+import Button from "./atoms/Button";
+
+export { Button };
