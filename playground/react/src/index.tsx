14da41031b307ec00c2a3187bcc208cd23db2b5d
sksurendrakmr <sksurendrakmr@gmail.com>
2022-08-21 20:58:29 +0530

    setup and configure playground to test the components defined inside packages

---
+++ playground/react/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Button } from '@firstmonorepo/demo'
+
+ReactDOM.render(<Button label='Hi' />, document.querySelector('#root'))
